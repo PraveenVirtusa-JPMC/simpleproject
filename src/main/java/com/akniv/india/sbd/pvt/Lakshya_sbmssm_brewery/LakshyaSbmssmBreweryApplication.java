cafad8c0dd1efcf80e6b2151f08119bf00872e2a
Praveen Verna IT <praveen.raparthi0825@yahoo.com>
2025-03-12 09:40:45 +0530

    March 12 small changes by praveen

---
+++ src/main/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/LakshyaSbmssmBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.akniv.india.sbd.pvt.Lakshya_sbmssm_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakshyaSbmssmBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakshyaSbmssmBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/LakshyaSbmssmBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akniv.india.sbd.pvt.Lakshya_sbmssm_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakshyaSbmssmBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
