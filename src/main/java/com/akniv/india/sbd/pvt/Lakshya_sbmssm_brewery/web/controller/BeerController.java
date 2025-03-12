47b582bb96d8f39990190d8b8e09e28416c4e1fb
Praveen Verna IT <praveen.raparthi0825@yahoo.com>
2025-03-12 15:36:38 +0530

    controller completed

--- src/main/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/web/controller/BeerController.java
+++ src/main/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/web/controller/BeerController.java
@@ -1,4 +1,20 @@
 package com.akniv.india.sbd.pvt.Lakshya_sbmssm_brewery.web.controller;
 
+import com.akniv.india.sbd.pvt.Lakshya_sbmssm_brewery.web.model.BeerDto;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
 public class BeerController {
+    @GetMapping({"/{beerId}"})
+ public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatusCode.OK);
+ }
 }
