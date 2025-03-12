e497a689a3106512193bffc424d7ccfd4edb97a6
Praveen Verna IT <praveen.raparthi0825@yahoo.com>
2025-03-12 10:12:46 +0530

    add variables.

--- src/main/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/web/model/BeerDto.java
+++ src/main/java/com/akniv/india/sbd/pvt/Lakshya_sbmssm_brewery/web/model/BeerDto.java
@@ -1,4 +1,10 @@
 package com.akniv.india.sbd.pvt.Lakshya_sbmssm_brewery.web.model;
 
+import java.util.UUID;
+
 public class BeerDto {
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
 }
